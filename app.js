220028d5227446fb931403e5a84954f2a95465f2
Juan <juan.ramonej@gmail.com>
2018-05-28 01:57:08 -0400

    adding init function in app.js

--- app.js
+++ app.js
@@ -11,9 +11,7 @@ GAME RULES:
 
 var scores, roundScore, activePlayer,dice, count;
 
-scores=[0,0];
-roundScore=0;
-activePlayer=1;
+init();
 
 count=0;
 // dice = Math.floor(Math.random() * 6) + 1;
@@ -26,12 +24,7 @@ count=0;
 // var x= document.querySelector('#score-0').textContent;
 // console.log(x);
 
-document.querySelector('.dice').style.display='none';
 
-document.getElementById('score-0').textContent='0';
-document.getElementById('current-0').textContent='0';
-document.getElementById('score-1').textContent='0';
-document.getElementById('current-1').textContent='0';
 
 
 document.querySelector('.btn-roll').addEventListener('click',function(){
@@ -76,8 +69,8 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
 
 });
 
+//change to my Next Player
 function nextPlayer(){
-    //Next Player
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
 
@@ -88,4 +81,32 @@ function nextPlayer(){
     document.querySelector('.player-1-panel').classList.toggle('active');
 
     document.querySelector('.dice').style.display = 'none';
+}
+
+// Start a New Game
+document.querySelector('.btn-new').addEventListener('click',init);
+
+//Reset my Values
+function init() {
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 1;
+
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('#name-0').textContent='Player 1';
+    document.querySelector('#name-1').textContent='Player 2';
+
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+
+    document.querySelector('.player-0-panel').classList.add('active');
 }
